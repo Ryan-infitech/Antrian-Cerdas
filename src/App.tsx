6284c82a75182bd5f72ac89717ec79209cb69b8d
Rian Septiawan <rianseptiawan@infitech.or.id>
2025-03-26 00:13:57 +0700

    route for join queue bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import ErrorBoundary from './components/ErrorBoundary';
-import Home from './pages/Home';
-import CreateQueue from './pages/CreateQueue';
-import JoinQueue from './pages/JoinQueue';
-import ManageQueue from './pages/ManageQueue';
-import ViewQueue from './pages/ViewQueue';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import ErrorBoundary from "./components/ErrorBoundary";
+import Home from "./pages/Home";
+import CreateQueue from "./pages/CreateQueue";
+import JoinQueue from "./pages/JoinQueue";
+import ManageQueue from "./pages/ManageQueue";
+import ViewQueue from "./pages/ViewQueue";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<CreateQueue />} />
+            <Route path="/join" element={<JoinQueue />} />
             <Route path="/join/:queueId" element={<JoinQueue />} />
             <Route path="/manage/:queueId" element={<ManageQueue />} />
             <Route path="/view/:queueId" element={<ViewQueue />} />
